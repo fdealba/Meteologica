1adc00d9a448a05386edc606f0b04c05e3ee4299
felipe de alba <felipe.de.alba.alegre@gmail.com>
2020-01-29 19:45:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import YAML from 'yaml';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { json: {} }
+
+  componentDidMount() {
+    this.readYAML('data.yml');
+  }
+
+  readYAML = (fileName) => {
+    fetch(fileName)
+    .then(response => response.text())
+    .then(content => this.setState({ json: YAML.parse(content) }));
+  }
+
+  render() {
+    const { json } = this.state;
+
+    return(
+      <p>
+        {JSON.stringify(json, null, 2)}
+      </p>
+    );
+  }
 }
 
 export default App;
